15679c2c5a4eba3207e0b447f5f2e6a7491d3c73
JINGCW <1562359870@qq.com>
2020-03-05 23:07:44 +0800

    HMSET

--- src/dego-redis/redis-hash.go
+++ src/dego-redis/redis-hash.go
@@ -1,1 +1,16 @@
 package main
+
+import "github.com/go-redis/redis/v7"
+
+func HashEX(addrp, passwd string) error {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addrp,
+		Password: passwd,
+		DB:       0,})
+	client.HMSet("hash_ex", "name", "redis tutorial", "description", "redis basic commands for caching", "likes", 20, "visitors", 2)
+	err := client.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
