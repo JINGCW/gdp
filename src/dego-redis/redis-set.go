543a695ef73f73da3684cc7da424ff1210a63789
JINGCW <1562359870@qq.com>
2020-03-08 23:14:27 +0800

    redis set

--- src/dego-redis/redis-set.go
+++ src/dego-redis/redis-set.go
@@ -1,1 +1,16 @@
 package dego
+
+import "github.com/go-redis/redis/v7"
+
+func SetEx(addr, passwd string) error {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: passwd,
+		DB:       0,
+	})
+	err := client.SAdd("set_ex", "mongdb", "mysql").Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
