7c18bade4ddda0faa8f732b5332ec58ec7133662
JINGCW <1562359870@qq.com>
2020-03-10 22:44:03 +0800

    redis zset

--- src/dego-redis/redis-zset.go
+++ src/dego-redis/redis-zset.go
@@ -1,1 +1,19 @@
 package dego
+
+import "github.com/go-redis/redis/v7"
+
+func ZsetEX(addr, passwd string) error {
+	pool := redis.NewClient(&redis.Options{
+		Addr:         addr,
+		Password:     passwd,
+		DB:           0,
+		MinIdleConns: 2,
+	})
+
+	out, err := pool.ZAdd("zset_ex", &redis.Z{1, "redis"}, &redis.Z{2, "mongodb"}).Result()
+	println(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
