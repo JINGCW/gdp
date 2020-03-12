aadae7dd79ed30866575a88a178445829ba04cc8
JINGCW <1562359870@qq.com>
2020-03-12 21:41:35 +0800

    pubsub

--- src/dego-redis/pubsub.go
+++ src/dego-redis/pubsub.go
@@ -1,5 +1,24 @@
 package dego
 
-func SubPub(addr,passwd string)error{
-	return nil
+import (
+	"fmt"
+	"github.com/go-redis/redis/v7"
+)
+
+func SubPub(addr, passwd string) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: passwd,
+		DB:       0,
+	})
+
+	done := make(chan struct{})
+	client.Publish("mychannel", "hello!\n")
+	go func() {
+		pubsub := client.Subscribe("mychannel")
+		msg, _ := pubsub.Receive()
+		fmt.Println("receive from channel", msg)
+		done <- struct{}{}
+	}()
+	<-done
 }
