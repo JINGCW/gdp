45eb81c7419036d6556bd8fa111aa5aa1f456158
JINGCW <1562359870@qq.com>
2020-03-15 23:27:40 +0800

    writer/reader

---
+++ deracy/racy.go
@@ -0,0 +1,43 @@
+package deracy
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
+func TimerRacy() {
+	start := time.Now()
+	var t *time.Timer
+
+	t = time.AfterFunc(randomDuration(),
+		func() {
+			fmt.Println(time.Now().Sub(start))
+			t.Reset(randomDuration())
+		})
+	time.Sleep(5 * time.Second)
+}
+
+func TimerRacySafe() {
+	start := time.Now()
+	reset := make(chan bool)
+	var t *time.Timer
+
+	t = time.AfterFunc(randomDuration(),
+		func() {
+			fmt.Println(time.Now().Sub(start))
+			reset <- true
+		})
+	for time.Since(start) < 5*time.Second {
+		<-reset
+		t.Reset(randomDuration())
+	}
+}
+
+func randomDuration() time.Duration {
+	dura := time.Duration(rand.Int63n(1e9))
+	println("dura: ", dura)
+	io.Copy()
+	return dura
+}
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import "hash_collections/deracy"
 
 //
 //const (
@@ -20,7 +20,8 @@ import "fmt"
 //const MaxUintptr = ^uintptr(0)
 
 func main() {
-	fmt.Printf("%x\n",-1&-1)
+	deracy.TimerRacySafe()
+	//fmt.Printf("%x\n",-1&-1)
 	//desync.MuDemo()
 	//config.ReadInConfig()
 	//println(viper.GetString("host_port"))
