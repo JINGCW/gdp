16218a56f26cd8db0dba792d58d30bf7cb484234
JINGCW <1562359870@qq.com>
2020-04-21 23:27:13 +0800

    ci status box

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # gdp
 ![Gopher image](doc/pictures/fiveyears.jpg)
 
-[![Build Status](https://travis-ci.org/JINGCW/gdp.png?branch=develop)](https://travis-ci.org/JINGCW/gdp)
 [![GoDoc](https://godoc.org/github.com/JINGCW/gdp?status.svg)](https://godoc.org/github.com/JINGCW/gdp)
 [![Coverage Status](https://coveralls.io/repos/github/JINGCW/gdp/badge.svg)](https://coveralls.io/github/JINGCW/gdp)
-[![Build status](https://ci.appveyor.com/api/projects/status/glj3j9c3oy8ekbic/branch/develop?svg=true)](https://ci.appveyor.com/project/JINGCW/gdp/branch/develop)
 
 Platform | CI status
 ---------|:---------
 OSX      |[![OSX Build Status](http://badges.herokuapp.com/travis/JINGCW/gdp?env=BADGE=osx&label=build&branch=develop&go=1.13.x)](https://travis-ci.org/JINGCW/gdp)<br/>[![OSX Build Status](http://badges.herokuapp.com/travis/JINGCW/gdp?env=BADGE=osx&label=build&branch=develop&go=1.14.x)](https://travis-ci.org/JINGCW/gdp)
 Linux    |[![Linux Build Status](http://badges.herokuapp.com/travis/JINGCW/gdp?env=BADGE=linux&label=build&branch=develop&go=1.13.x)](https://travis-ci.org/JINGCW/gdp)<br/>[![Linux Build Status](http://badges.herokuapp.com/travis/JINGCW/gdp?env=BADGE=linux&label=build&branch=develop&go=1.14.x)](https://travis-ci.org/JINGCW/gdp)
+Windows  |[![Build status](https://ci.appveyor.com/api/projects/status/glj3j9c3oy8ekbic/branch/develop?svg=true)](https://ci.appveyor.com/project/JINGCW/gdp/branch/develop)
+
 API docs: https://godoc.org/github.com/JINGCW/gdp.
 
 Examples: https://godoc.org/github.com/JINGCW/gdp#pkg-examples.
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "hash_collections/src/asm_x86/g"
-
 //
 //const (
 //	mask uintptr = 1<<5 - 1
@@ -22,66 +20,4 @@ import "hash_collections/src/asm_x86/g"
 //}
 
 func main() {
-	g1,g2:=g.G_innerouter()
-	println(g1,g2)
-	//fmt.Println(add_sub_mul.Output2(3,4))
-	//gid.GoI
-	//D()
-	//var m = map[int]int{}
-	//m[43] = 1
-	//var n = map[string]int{}
-	//n["abc"] = 1
-	//println(m, n)
-	//fmt.Println(add_sub_mul.ArraySum([]int64{12, 3, 4}))
-	//deracy.SockServer()
-	//deracy.SockClient()
-	//fmt.Printf("%x\n",-1&-1)
-	//desync.MuDemo()
-	//config.ReadInConfig()
-	//fmt.Println(viper.GetStringSlice("cluster_hp"))
-	//_=dego.Cclient(viper.GetStringSlice("cluster_hp"), viper.GetString("passwd"))
-	//if e!=nil{
-	//	panic(e)
-	//}
-	//s:=dego_redis.Usr{"hello","123@qq.com",123}
-	//bytes,err:=json.Marshal(s)
-	//if err!=nil{
-	//	panic(err)
-	//}
-	//out:=dego_redis.Usr{}
-	//_=json.Unmarshal(bytes,&out)
-	//fmt.Printf("%v",reflect.Indirect(reflect.ValueOf(out)).Kind())
-	//fmt.Printf("%v",out)
-
-	// This input can come from anywhere, but typically comes from
-	// something like decoding JSON where we're not quite sure of the
-	// struct initially.
-	//input := map[string]interface{}{
-	//	"name":   "Mitchell",
-	//	"age":    91,
-	//	"emails": []string{"one", "two", "three"},
-	//	"extra": map[string]string{
-	//		"twitter": "mitchellh",
-	//	},
-	//}
-
-	//var result Person=Person{
-	//
-	//}
-	//dataValKind:=dataValType.Key().Kind()
-
-	//var r1 reader
-	//r1=&concreate_type{"haha"}
-	//fmt.Println(reflect.ValueOf(iv).Elem())
-	//var iv type_interface=&concreate_type{"somthing"}
-	//var eface interface{}= (interface{})(*(*interface {
-	//	M()string
-	//})(unsafe.Pointer(reflect.ValueOf(err).Pointer())))
-	//var r2 rwer=r1
-	//out:="abcd"
-	//out2:=dereflect.ValueOf(out)
-	//fmt.Println(unsafe.Sizeof(out))
-	//fmt.Println(unsafe.Sizeof(out2))
-	//fmt.Printf("%x", temp)
-	//fmt.Println(HashStr("abc"))
 }
